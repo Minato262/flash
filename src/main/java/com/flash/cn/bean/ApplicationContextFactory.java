4d161c459edbfed1339fe6a83b86f12c92b29b13
kay <1132892414@qq.com>
2017-09-16 00:45:27 +0800

    调整代码结构。

--- src/main/java/com/flash/cn/bean/ApplicationContextFactory.java
+++ src/main/java/com/flash/cn/bean/ApplicationContextFactory.java
@@ -4,15 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Application Context Factory.
+ *
  * @author kay
  * @version v1.0
  */
 public class ApplicationContextFactory implements BeanFactory {
 
-    private Map beanContainer = new HashMap();
+    private Map container = new HashMap();
 
+    /**
+     * get Bean.
+     *
+     * @param requiredType the required of type
+     * @param <T>          new object
+     * @return bean object
+     */
     @Override
-    public void init() {
-
+    public <T> T getBean(Class<T> requiredType) {
+        return null;
     }
 }
--- src/main/java/com/flash/cn/bean/BeanFactory.java
+++ src/main/java/com/flash/cn/bean/BeanFactory.java
@@ -1,10 +1,19 @@
 package com.flash.cn.bean;
 
 /**
+ * Bean Factory.
+ *
  * @author kay
  * @version v1.0
  */
 public interface BeanFactory {
 
-    void init();
+    /**
+     * get Bean.
+     *
+     * @param requiredType the required of type
+     * @param <T>          new object
+     * @return bean object
+     */
+    <T> T getBean(Class<T> requiredType);
 }
