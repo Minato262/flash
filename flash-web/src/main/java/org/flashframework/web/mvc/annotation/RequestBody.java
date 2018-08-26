25e21193e1db014d1b9c689e1b8d6d63d395c16a
kay <1132892414@qq.com>
2018-08-26 22:58:30 +0800

    添加 RequestBody 注解.
    Signed-off-by: kay <1132892414@qq.com>

---
+++ flash-web/src/main/java/org/flashframework/web/mvc/annotation/RequestBody.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flashframework.web.mvc.annotation;
+
+import org.flashframework.beans.annotation.Controller;
+
+import java.lang.annotation.*;
+
+/**
+ * {@code RequestBody} 注解的作用是将 {@code Controller} 的方法返回的对象通过适
+ * 当的转换器转换为指定的格式之后，写入到 response 对象的 body 区，通常用来返回 JSON
+ * 数据或者是 XML.
+ *
+ * @author kay
+ * @version v2.0
+ * @see Controller
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface RequestBody {
+}
