9dcfb679c624006cb42ebc26a3100f68a39720fc
kay <1132892414@qq.com>
2018-10-06 01:08:47 +0800

    添加 AOP 注解
    Signed-off-by: kay <1132892414@qq.com>

---
+++ flash-core/src/test/java/org/flashframework/LogTest.java
@@ -0,0 +1,25 @@
+package org.flashframework;
+
+import org.junit.Test;
+
+/**
+ * @author kay
+ * @version v1.0
+ */
+public class LogTest {
+
+    @Test
+    public void test() {
+        //
+    }
+
+    @Test
+    public void test1() {
+        //
+    }
+
+    @Test
+    public void test2() {
+        //
+    }
+}
