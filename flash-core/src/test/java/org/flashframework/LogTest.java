f10a0368f6f25b6eb1a422e4dc362400edfa9754
kay <1132892414@qq.com>
2018-10-06 01:13:54 +0800

    添加 log 设置
    Signed-off-by: kay <1132892414@qq.com>

--- flash-core/src/test/java/org/flashframework/LogTest.java
+++ flash-core/src/test/java/org/flashframework/LogTest.java
@@ -1,6 +1,9 @@
 package org.flashframework;
 
+import org.flashframework.core.config.LogConfiguration;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author kay
@@ -8,9 +11,20 @@ import org.junit.Test;
  */
 public class LogTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(LogTest.class);
+
     @Test
     public void test() {
-        //
+        LogConfiguration.initLog();
+        logger.debug("log debug");
+        logger.info("log info");
+        logger.warn("log warn");
+        logger.error("log error");
+
+        logger.debug("log1 debug");
+        logger.info("log1 info");
+        logger.warn("log1 warn");
+        logger.error("log1 error");
     }
 
     @Test
