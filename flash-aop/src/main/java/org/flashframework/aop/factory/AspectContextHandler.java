535ef0ea2c559ffcfeb13256771684e9974e6065
kay <1132892414@qq.com>
2019-04-05 08:30:48 +0800

    init aop.

--- flash-aop/src/main/java/org/flashframework/aop/factory/AspectContextHandler.java
+++ flash-aop/src/main/java/org/flashframework/aop/factory/AspectContextHandler.java
@@ -39,7 +39,7 @@ public class AspectContextHandler extends ApplicationContextHandler {
      * <p>默认载入 Aspect 注解</p>
      *
      * @param clazz class 信息
-     * @throws IllegalArgumentException 如果Class为null，则抛出异常
+     * @throws IllegalArgumentException 如果 Class 为null，则抛出异常
      */
     @Override
     public void load(Class clazz) {
@@ -68,22 +68,27 @@ public class AspectContextHandler extends ApplicationContextHandler {
             Before before = method.getAnnotation(Before.class);
             if (before != null) {
                 handler.setBefore(before);
+                continue;
             }
             Around around = method.getAnnotation(Around.class);
             if (around != null) {
                 handler.setAround(around);
+                continue;
             }
             After after = method.getAnnotation(After.class);
             if (after != null) {
                 handler.setAfter(after);
+                continue;
             }
             Pointcut pointcut = method.getAnnotation(Pointcut.class);
             if (pointcut != null) {
                 handler.setPointcut(pointcut);
+                continue;
             }
             AfterReturning afterReturning = method.getAnnotation(AfterReturning.class);
             if (afterReturning != null) {
                 handler.setAfterReturning(afterReturning);
+                continue;
             }
             AfterThrowing afterThrowing = method.getAnnotation(AfterThrowing.class);
             if (afterThrowing != null) {
--- flash-aop/src/main/java/org/flashframework/aop/handle/method/MethodInterceptor.java
+++ flash-aop/src/main/java/org/flashframework/aop/handle/method/MethodInterceptor.java
@@ -1,7 +1,5 @@
 package org.flashframework.aop.handle.method;
 
-import org.flashframework.aop.annotation.Annotation;
-
 import java.lang.reflect.Method;
 
 public interface MethodInterceptor {
